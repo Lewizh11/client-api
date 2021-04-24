af9c445d52b76ed07c4548e0ffb94aaa11c3db26
Lewis <luizh11tmj@gmail.com>
2021-04-23 21:10:40 -0300

    Upload sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+arch=uname -m
+pkg update && pkg install wget
+case $arch in aarch64)
+  wget https://raw.githubusercontent.com/Lewizh11/client-api/main/builds/aarch-android/aarch && chmod 777 aarch && ./aarch
+;;
+armv7l)
+  wget https://raw.githubusercontent.com/Lewizh11/client-api/main/builds/arm-android/armv && chmod 777 armv && ./armv
+;;
+esac
